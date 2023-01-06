1157569434bd4e2850e710ff511177dd7f30ca77
Aaron Chris <rooneychris83@gmail.com>
2023-01-06 16:35:42 +0100

    ooooohhhhhhhh!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,23 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
+  theme: {
+    colors: {
+      'lightdark':'#666666'
+    },
+  },
     content: ['./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'],
     plugins: [
       require('daisyui'),
     ],
     daisyui: {
         styled: true,
-        themes: false,
+        themes: true,
         base: false,
         utils: true,
         logs: true,
         rtl: false,
         prefix: "",
-        darkTheme: "dark",
+        darkTheme: "black",
       },
+      themes: ["black"],
   }
