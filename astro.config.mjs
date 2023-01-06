5a01fb82d498e1fee1858de4196dbdaf5caa1333
Aaron Chris <rooneychris83@gmail.com>
2023-01-06 11:44:28 +0100

    made utils

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import daisyui from 'daisyui'
+import { remarkReadingTime } from './src/utils/reading-time.mjs';
 // https://astro.build/config
 
 
 export default defineConfig({
+
+
+    markdown: {
+      extendDefaultPlugins: true,
+      remarkPlugins: [remarkReadingTime],
+       shikiConfig:{
+        theme: "vitesse-dark"
+       }
+    },
       integrations: [mdx(), tailwind({
         config: { path: './tailwind.config.cjs' },
         plugins: [daisyui],
---
+++ src/utils/reading-time.mjs
@@ -0,0 +1,13 @@
+import getReadingtime from 'reading-time';
+import { toString } from 'mdast-util-to-string';
+
+
+
+export const remarkReadingTime = () => {
+    return (tree, {data}) => {
+        const textOnPage = toString(tree)
+        const readingTime = getReadingtime(textOnPage)
+
+        data.astro.frontmatter.readingTime = readingTime.text
+    }
+}
