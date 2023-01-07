22d7e29447de2a907ce4c947848a6924c11bccbe
Aaron Chris <rooneychris83@gmail.com>
2023-01-07 15:52:21 +0100

    added mdx and code highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,25 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import daisyui from 'daisyui'
+import CodeTitle from "remark-code-title"
+import astroLayouts from "astro-layouts"
 import { remarkReadingTime } from './src/utils/reading-time.mjs';
 // https://astro.build/config
 
+
+const layoutOptions = {
+  "pages/blog/**/*": "@layouts/BlogLayout.astro",
+}
 
 export default defineConfig({
 
 
     markdown: {
       extendDefaultPlugins: true,
-      remarkPlugins: [remarkReadingTime],
+      remarkPlugins: [[astroLayouts, layoutOptions],remarkReadingTime],
        shikiConfig:{
-        theme: "vitesse-dark"
-       }
+        theme: "dark-plus"
+       },
     },
       integrations: [mdx(),react(), tailwind({
         config: { path: './tailwind.config.cjs' },
