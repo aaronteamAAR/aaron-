84dfdd99db73500504602715e885f2227cee6dd2
Aaron Chris <rooneychris83@gmail.com>
2023-01-13 10:19:58 +0100

    added tech stacks!

---
+++ src/utils/stacks.ts
@@ -0,0 +1,86 @@
+const technologies = [
+    {
+        name: 'Astro',
+        imgSrc: 'https://bestofjs.org/logos/astro.dark.svg',
+        altSrc: 'Astro Logo',
+        link: 'https://astro.build/',
+    },
+    {
+        name: 'JavaScript',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png',
+        altSrc: 'JavaScript Logo',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    },
+    {
+        name: 'Node.js',
+        imgSrc: 'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png',
+        altSrc: 'Node.js Logo',
+        link: 'https://nodejs.org/en/',
+    },
+    {
+        name: 'Python',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png',
+        altSrc: 'Python Logo',
+        link: 'https://www.python.org/',
+    },
+    {
+        name: 'React',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png',
+        altSrc: 'React Logo',
+        link: 'https://reactjs.org/',
+    },
+    {
+        name: 'TypeScript',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png',
+        altSrc: 'TypeScript Logo',
+        link: 'https://www.typescriptlang.org/',
+
+    },
+    {
+        name: 'Tailwind CSS',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg',
+        altSrc: 'Tailwind CSS Logo',
+        link: 'https://tailwindcss.com/',
+    },
+    {
+        name: 'Next.js',
+        imgSrc: 'https://static.netify.ai/logos/v/e/r/irepry/icon.png?v=1',
+        altSrc: 'Next.js Logo',
+        link: 'https://nextjs.org/',
+    }
+]
+
+const socials = [
+    {
+        name: 'Twitter',
+        url: 'https://twitter.com/soysarcasme',
+        imgSrc: 'mdi:twitter',
+    },
+    {
+        name: 'GitHub',
+        url: 'https://github.com/dreyfus92',
+        imgSrc: 'mdi:github',
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/paul-valladares',
+        imgSrc: 'mdi:linkedin',
+    },
+    {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/sarcasme92',
+        imgSrc: 'mdi:instagram',
+    },
+    {
+        name: 'Discord',
+        url: 'https://discord.com/users/603517171175391242',
+        imgSrc: 'ri:discord-fill',
+    },
+    {
+        name: 'Frontend Mentor',
+        url: 'https://www.frontendmentor.io/profile/dreyfus92',
+        imgSrc: 'simple-icons:frontendmentor',
+    },
+]
+
+export { technologies, socials }
